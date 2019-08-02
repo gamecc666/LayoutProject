a5228323c4b9867a931d86585e583b22bc50fe10
刘跃灿 <995611928@qq.com>
2019-08-02 12:21:45 +0800

    init

---
+++ SpecialTestProject/Controllers/MainRightController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpecialTestProject.Controllers
+{
+    public class MainRightController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SpecialTestProject/Controllers/MianFrameController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpecialTestProject.Controllers
+{
+    public class MianFrameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
